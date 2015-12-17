6f34e6dacc70f653b3a3ed227429649723c59841
riQe <henrique_webdesigner@hotmail.com>
2015-12-17 21:32:50 -0200

    new past used to files controllers of the system

---
+++ controllers/DB.php
@@ -0,0 +1,35 @@
+<?php 
+#include config.php
+require_once 'config.php'
+
+class DB {
+
+	private static $instance;
+
+	public static function getInstance() {
+		# Se não existir valor na $instance, passa valores de conexao
+		if (!isset(self::$instance)) {
+
+			try {
+
+				self::$instance = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPETION);
+				self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJECT);
+				self::$instance-> exec("SET CHARACTER SET utf8");
+			} catch (PDOExcepetion $e) {
+
+				echo $e->getMessage();
+
+			}
+		}
+	#retorna os valores da variavel $instance	
+	return self::$instance;	
+
+	}
+	# inclui a linha de crud sql na conexao ao banco de dados
+	public static function prepare($qry) {
+		return self::getInstance()->prepare($qry);
+	}
+}
+	
+?>
---
+++ controllers/config.php
@@ -0,0 +1,10 @@
+<?php 
+	
+#define valores para ser recuperados no arquivo DB.php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'systemvideo');
+define('DB_USER', 'myroot');
+define('DB_PASS', '0808hrq');
+
+?>
