63c286d6d599ed5f5b5ae89bbf172e38f0166dd8
riQe <henrique_webdesigner@hotmail.com>
2016-01-01 10:48:03 -0200

    add

---
+++ main/laytest.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf8">
+	<title>Test</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+<!--Fixed menu with logo-->		
+	<header>
+	<!--begin div Logo-->
+		<div id="logo">
+			<h1>
+			Just<span class="acolor">A</span>
+			<span class="salt"><p>name<p></span>
+			</h1>
+		</div>
+	<!--end of div logo-->
+	<nav id="topmenu">
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+	</header>
+<!--After menu-->
+	<section>
+		<article>
+			<div id="box1">
+				<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+			</div>
+		</article>
+	</section>
+
+</body>
+</html>
