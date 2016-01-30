5dcc33d4542a88f1898999fbe82ae811f575bce3
Henrique <henrique_webdesigner@hotmail.com>
2016-01-30 09:49:36 -0200

    1

--- controllers/config.php
+++ controllers/config.php
@@ -3,7 +3,7 @@
 #define valores para ser recuperados no arquivo DB.php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'systemvideo');
+define('DB_NAME', 'json_teste');
 define('DB_USER', 'myroot');
 define('DB_PASS', '0808hrq');
 
